dfdc102e03a60b56c375820116b8fd91818d0256
badjelix <miguel.ptfg@hotmail.com>
2021-03-21 20:05:27 +0000

    add bots

---
+++ bot1.py
@@ -0,0 +1,23 @@
+# bot.py
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print('Bot is up.')
+
+client.run(TOKEN)
